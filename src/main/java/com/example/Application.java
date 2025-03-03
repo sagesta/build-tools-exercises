2951eb21ea7e4c046a4b1f8fff452114441b64c9
sagesta <adebodun93@gmail.com>
2025-03-03 11:44:34 +0100

    fixed application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -10,15 +10,21 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        // add your code snippet here
+
+        Logger log = LoggerFactory.getLogger(Application.class);
+        try {
+            String one = args[0];
+            String two = args[1];
+            log.info("Application will start with the parameters {} and {}", one, two);
+        } catch (Exception e) {
+            log.info("No parameters provided");
+        }
     }
 
     @PostConstruct
-    public void init()
-    {
+    public void init() {
         Logger log = LoggerFactory.getLogger(Application.class);
         log.info("Java app started");
     }
